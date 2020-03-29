14cd9d87fb78df12026d99c097d76b7d4e0ff666
Rahul Prabhu <rahul.r.prabhu@gmail.com>
2020-03-29 09:52:30 +0530

    Setup.py readme error

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='Rahul Prabhu',
     author_email='grokwithahul@gmail.com',
     description='Levenshtein project for python',
-    long_description=open("README.md"),
+    long_description=open("README.md").read(),
     project_urls={
         "Source": "https://github.com/Redstomite/py-levenshtein",
         "Say Thanks!": "https://saythanks.io/to/grokwithrahul%40gmail.com",
